7a0d2f94d694b380651a28edc03a219f1837f118
frankgomezdev <franklingomezdev@gmail.com>
2025-02-27 17:06:00 -0500

    created transactionlist and transactionitem component and gettransaction action

---
+++ components/TransactionItem.tsx
@@ -0,0 +1,19 @@
+import { Transaction } from "@/types/Transactions";
+import { addCommas } from "@/lib/utils";
+import { toast } from "react-toastify";
+
+
+const TransactionItem = ({ transaction}: { transaction: Transaction}) => {
+
+    const sign = transaction.amount < 0 ? '-': '+';
+  return (
+    <li className={ transaction.amount < 0 ? 'minus': 'plus'}>
+        {transaction.text}
+        <span>
+           {sign}${ addCommas(Math.abs(transaction.amount))}
+        </span>
+    </li>
+  )
+}
+
+export default TransactionItem
